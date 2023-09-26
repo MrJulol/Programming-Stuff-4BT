7e7f3b4238566393e2a76d79091ed67abae0147f
MrJulol <julian.thaler06@gmail.com>
2023-09-26 09:24:10 +0200

    imrove OO3_A10 baumarkt. Now user friendly

---
+++ Info/OO3_A10/src/Prints.java
@@ -0,0 +1,40 @@
+public class Displays {
+
+    public String thankYou(){
+        return "Thank you for Buying!";
+    }
+
+    public String buyQuestion() {
+        return "Do you want to buy? 1 = yes, 0 = no";
+    }
+
+    public void showExpSwitch1() {
+        System.out.println("Select mode to set values to everything");
+        System.out.println("\t|1| = Set to default values");
+        System.out.println("\t|2| = Set to custom values");
+        System.out.println("\trest = stop");
+
+    }
+
+    public String userInputInt() {
+        return "Enter a number (int) ";
+    }
+
+    public String userInputString() {
+        return "Enter a Word (String) ";
+    }
+
+    public void setProductsNameQuestion() {
+        System.out.println("Enter the name of a product");
+    }
+
+    public void setProductsPriceQuestion() {
+        System.out.println("Enter the price of a product");
+    }
+
+    public void BuyMenu(Produkt[] products) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Product " + i + ": name = " + products[i].getName() + " ||Price = " + products[i].getPrice());
+        }
+    }
+}
