291395c2c7601b3bdcf95d2853debd826bffd9ca
MrJulol <125541580+MrJulol@users.noreply.github.com>
2023-10-25 07:01:06 +0000

    TP

---
+++ TP/matrix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int m1[3][4] = {{1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
+    int m2[3][4] = {{5,6,7,8},{5,6,7,8},{5,6,7,8}};
+
+    int m3[3][3];
+
+    printf("start!\n");
+    return EXIT_SUCCESS;
+}
